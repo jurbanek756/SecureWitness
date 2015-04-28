88b622f48b05a18bc95c54c690d15ae474fb8275
mjtesema <mjt3hb@virginia.edu>
2015-04-27 21:09:41 -0400

    resync db for heroku

--- SecureWitness/SecureWitness/settings.py
+++ SecureWitness/SecureWitness/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-import dj_database_url
+
 
 '''
 Things to Change Back:
